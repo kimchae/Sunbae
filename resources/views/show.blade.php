19d7cc02032f1c8b6bc79156d8ad3a9e624c5e9a
ddolpali <matt@aegyo.pro>
2017-03-25 12:28:01 -0700

    finish show page

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -4,14 +4,54 @@
 	<div class="row">
 		<div class="col s12">
 			<div class="card-panel content">
-				<div class="col m8">
+				<div class="hide-on-med-and-up">
+					<img src="/img/covers/{{ $show->id }}.jpg" width="100%">
 					<div class="titleSpace">
 						<h4 style="margin-bottom: 0">{{ $show->name }}</h4>
 						<small>{{ $show->altname }}</small>
 					</div>
+					<p>{{ $show->synopsis }}</p>
 				</div>
-				<div class="col m4">
-					<img src="/img/posters/{{ $show->id }}.jpg" width="100%">
+				<div class="row">
+					<div class="col m9 hide-on-small-only">
+						<div class="titleSpace">
+							<h4 style="margin-bottom: 0">{{ $show->name }}</h4>
+							<small>{{ $show->altname }}</small>
+						</div>
+						<p>{{ $show->synopsis }}</p>
+					</div>
+					<div class="col m3 hide-on-small-only">
+						<img src="/img/posters/{{ $show->id }}.jpg" width="100%">
+					</div>
+				</div>
+				<div class="ep-content">
+					<h5 class="center">Episodes</h5>
+					<table class="highlight responsive-table">
+  						<thead>
+							<tr>
+								<th></th>
+								<th>Upload Date</th>
+								<th>Encoder</th>
+								<th>Uploader</th>
+							</tr>
+  						</thead>
+  						<tbody>
+							@foreach ($show->episodes as $episode)
+								<tr>
+		  							<td><a href="#">Episode {{ $episode->number }}
+										@if ($episode->subbed)
+											<span data-badge-caption="Subbed" class="new badge blue"></span>
+										@else
+											<span data-badge-caption="Raw" class="new badge red"></span>
+										@endif
+									</a></td>
+		  							<td>TBA</td>
+		  							<td>{{ $episode->encoder }}</td>
+									<td>{{ $episode->uploader }}</td>
+								</tr>
+							@endforeach
+  						</tbody>
+					</table>
 				</div>
 			</div>
 		</div>
